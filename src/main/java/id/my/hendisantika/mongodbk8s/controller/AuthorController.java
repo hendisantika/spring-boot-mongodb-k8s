c9649dd5f04ac540eb6c4e1fecfc7ec9f47ab838
hendisantika <hendisantika@yahoo.co.id>
2025-05-10 06:05:12 +0700

    feat: Add AuthorController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/mongodbk8s/controller/AuthorController.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.mongodbk8s.controller;
+
+import id.my.hendisantika.mongodbk8s.service.AuthorService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongodb-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/05/25
+ * Time: 06.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/author")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+}
