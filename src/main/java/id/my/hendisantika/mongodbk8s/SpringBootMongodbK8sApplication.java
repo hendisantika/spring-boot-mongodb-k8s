dd27db2a8c13b5559eb14fa758596a547608b0bc
hendisantika <hendisantika@yahoo.co.id>
2025-05-10 05:48:02 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/mongodbk8s/SpringBootMongodbK8sApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.mongodbk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/mongodbk8s/SpringBootMongodbK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.mongodbk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
