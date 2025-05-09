bdde59bf08071e4d680826427d65c6e4da57848a
hendisantika <hendisantika@yahoo.co.id>
2025-05-10 06:11:30 +0700

    feat: Add getVolumeFileContent in ConfigMapController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/mongodbk8s/controller/ConfigMapController.java
+++ src/main/java/id/my/hendisantika/mongodbk8s/controller/ConfigMapController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-mongodb-k8s
@@ -32,6 +39,16 @@ public class ConfigMapController {
     @GetMapping("/db-config")
     public DbInfo getStatus() {
         return new DbInfo(host, name, debug);
+    }
+
+    @GetMapping("/db-config-volume")
+    public List<String> getVolumeFileContent() {
+        String fileName = "/etc/db/app.properties";
+        try {
+            return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            return Collections.emptyList();
+        }
     }
 }
 
