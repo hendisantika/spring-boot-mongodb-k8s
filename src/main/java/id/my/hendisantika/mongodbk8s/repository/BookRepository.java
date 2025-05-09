50bf68faf9ba67902047ab3ca96c78b1bfd0ebcf
hendisantika <hendisantika@yahoo.co.id>
2025-05-10 05:54:06 +0700

    feat: Add BookRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/mongodbk8s/repository/BookRepository.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.mongodbk8s.repository;
+
+import id.my.hendisantika.mongodbk8s.model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongodb-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/05/25
+ * Time: 05.53
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface BookRepository extends MongoRepository<Book, String> {
+}
