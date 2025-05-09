f20a6bae06685b152dc7b5cb599b8e21eff34a1d
hendisantika <hendisantika@yahoo.co.id>
2025-05-10 06:10:30 +0700

    feat: Add ConfigMapController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/mongodbk8s/controller/ConfigMapController.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.mongodbk8s.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongodb-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/05/25
+ * Time: 06.10
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api")
+public class ConfigMapController {
+
+    @Value("${application.env.db-host}")
+    private String host;
+
+    @Value("${application.env.db-name}")
+    private String name;
+
+    @Value("${application.env.debug}")
+    private String debug;
+}
