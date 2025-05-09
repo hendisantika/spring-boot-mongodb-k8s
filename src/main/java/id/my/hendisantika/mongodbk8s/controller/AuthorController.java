74cbad97d685fec7c5ab78319d54552369c71a52
hendisantika <hendisantika@yahoo.co.id>
2025-05-10 06:05:47 +0700

    feat: Add createAuthor in AuthorController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/mongodbk8s/controller/AuthorController.java
+++ src/main/java/id/my/hendisantika/mongodbk8s/controller/AuthorController.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.mongodbk8s.controller;
 
+import id.my.hendisantika.mongodbk8s.model.Author;
 import id.my.hendisantika.mongodbk8s.service.AuthorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +30,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthorController {
 
     private final AuthorService authorService;
+
+    /**
+     * {@code POST  /author} : Create a new author.
+     *
+     * @param author the author to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new author.
+     */
+    @PostMapping()
+    public ResponseEntity<Author> createAuthor(@RequestBody @Valid Author author) {
+        log.debug("REST request to save Author : {}", author);
+        return new ResponseEntity<>(authorService.create(author), HttpStatus.CREATED);
+    }
 
 }
